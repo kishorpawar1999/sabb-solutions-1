09350b616512fee9c255a5a8637b4be1ba6ba198
kishorpawar1999 <109018667+kishorpawar1999@users.noreply.github.com>
2022-07-25 21:39:54 +0530

    Update Navbar.js

--- ss_frontend/src/components/Navbar/Navbar.js
+++ ss_frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo'>
-            hii
+            
           </Link>
         </div>
 
